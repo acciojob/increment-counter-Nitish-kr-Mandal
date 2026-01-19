a96e0c9b0e0d506901173f05b3bbf738bce6bd87
Nitish Kumar Mandal <147975324+Nitish-kr-Mandal@users.noreply.github.com>
2026-01-19 13:18:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let counter = document.getElementById('counter')
+let increment = document.getElementById('incrementBtn')
+
+let value = Number(counter.innerHTML)
+
+increment.addEventListener('click', () =>{
+	alert(value)
+	value++
+	counter.innerHTML = value
+})
